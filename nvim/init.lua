ce9debb7db8bc41e14064a7808d772953e19a7a1
Mike Thomas <mike@protonmail.com>
2026-09-03 18:39:03 +1200

    Updated config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,17 @@
 require("config.lazy")
+
+-- 4 spaces instead of tabs
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- navigate to previous/next line with left/right arrow keys
+vim.opt.whichwrap:append("<,>,[,]")
+
+-- Switch to next/previous buffer
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', { silent = true })
+
+-- Or use leader key combinations
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { silent = true })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { silent = true })
